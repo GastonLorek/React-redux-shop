526f358b66034d5954b08eaaebab99f8a0853755
Lorekgaston <lorekgaston@gmail.com>
2020-12-08 13:18:06 -0300

    nueva appBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,62 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import './Header.css';
 import ShoppingCartSharpIcon from '@material-ui/icons/ShoppingCartSharp';
+import { grey, green } from '@material-ui/core/colors';
 
-const Header = ({ handleShoppingCart }) => {
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  },
+  bar: {
+    background: grey[900]
+  },
+  inside: {
+    width: '1300px',
+    margin: '0 auto',
+    color: green['A200']
+  },
+  menuButton: {
+    marginRight: '10px'
+  }
+});
+
+const Header = () => {
+  const classes = useStyles();
   return (
-    <div className="header">
-      <span>Logo</span>
-      <div className="header__cart">
-        <ShoppingCartSharpIcon onClick={handleShoppingCart} />
-        <span>0</span>
-      </div>
+    <div className={classes.root}>
+      <AppBar position="relative" className={classes.bar}>
+        <Toolbar className={classes.inside}>
+          <Typography variant="h5" className={classes.title}>
+            React Shoes Store
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <ShoppingCartSharpIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
 
 export default Header;
+
+// <div className="header">
+//   <span>Logo</span>
+//   <div className="header__cart">
+//     <ShoppingCartSharpIcon onClick={handleShoppingCart} />
+//     <span>0</span>
+//   </div>
+// </div>
