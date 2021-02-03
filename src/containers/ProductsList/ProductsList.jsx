4f1025034c13972d8d6c23247217ff1009ca01f7
Lorekgaston <lorekgaston@gmail.com>
2021-02-03 14:33:44 -0300

    add redux thunk, fetch data from api

--- src/containers/ProductsList/ProductsList.jsx
+++ src/containers/ProductsList/ProductsList.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TransitionsModal from '../../components/Modal/Modal';
 import ProductCard from '../../components/ProductCard/ProductCard';
 
+import { fetchProducts } from '../../redux/actions/products';
 const useStyles = makeStyles(() => ({
   girdContainer: {
     minHeight: 'calc(100vh - 96px)',
@@ -30,24 +31,33 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
+const useFetchProducts = action => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(action);
+  }, []);
+};
+
 const ProductsList = () => {
-  const { products } = useSelector(state => state);
+  const { products, isLoading } = useSelector(state => state.products);
   const history = useHistory();
   const classes = useStyles();
 
   const handleCardAction = id => history.push(`/product/${id}`);
 
+  useFetchProducts(fetchProducts());
+
   const renderProducts = useMemo(
     () =>
-      products.map(product => {
-        const { name, image, price, id } = product;
+      products?.map(product => {
+        const { name, image, price, _id } = product;
         return (
           <ProductCard
             key={product.id}
             name={name}
             image={image}
             price={price}
-            id={id}
+            id={_id}
             onHandleCardAction={handleCardAction}
             classes={classes}
           />
@@ -58,10 +68,14 @@ const ProductsList = () => {
 
   return (
     <div className={classes.girdContainer}>
-      <Grid container item sm={12} spacing={2} className={classes.grid}>
-        {renderProducts}
-        <TransitionsModal />
-      </Grid>
+      {isLoading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <Grid container item sm={12} spacing={2} className={classes.grid}>
+          {renderProducts}
+          <TransitionsModal />
+        </Grid>
+      )}
     </div>
   );
 };
---
+++ src/redux/actions/products.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import * as actionType from '../CONSTANTS';
+
+const fetchProductRequest = () => {
+  return {
+    type: actionType.FETCH_PRODUCTS_REQUESTED
+  };
+};
+const fetchProductSucces = products => {
+  return {
+    type: actionType.FETCH_PRODUCTS_SUCCEDED,
+    products
+  };
+};
+const fetchProductFail = err => {
+  return {
+    type: actionType.FETCH_PRODUCTS_FAILED,
+    err
+  };
+};
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    dispatch(fetchProductRequest);
+    try {
+      const response = await axios.get(
+        'https://storenodejsapi.herokuapp.com/api/v1/getProducts'
+      );
+      const { data: { data: { products } = {} } = {} } = response || {};
+      dispatch(fetchProductSucces(products));
+    } catch (err) {
+      dispatch(fetchProductFail(err.message));
+    }
+  };
+};
